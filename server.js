24d7b5930ed6a4a9b1decc0f95d94dd1bcaa2f35
Ozurmba chibuike <ocbmf17@gmail.com>
2025-01-23 15:37:36 +0100

    new update added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://defiaccess.info',
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 
